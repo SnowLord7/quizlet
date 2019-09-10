0f6131ae8b62a420ebd88b83f153b792b096f914
SnowLord <31230715+SnowLord7@users.noreply.github.com>
2019-09-09 17:37:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -22,6 +22,14 @@ var defaultExtensionSettings = {
     gravityScore,
     href = window.location.href;
 
+function obfuscate(msg, num) {
+	var answer = "";
+	for (let i = 0; i < msg.length; i++) {
+		answer = answer + ("-" + (msg.charCodeAt(i) + num % (i + 1)));
+	}
+	return answer.slice(1);
+}
+
 function saveExtensionSettings(settings) {
     if (localStorage && typeof settings == 'object') localStorage.setItem('extensionSettings', JSON.stringify(settings));
 }

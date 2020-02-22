328e2e98c2ff60035f799f8b2338fe43d9383943
SnowLord <31230715+SnowLord7@users.noreply.github.com>
2020-02-21 20:38:09 -0800

    fixed learn... once more

--- main.js
+++ main.js
@@ -835,7 +835,7 @@ Learn.prototype.loop = function () {
 
 		alert('Please make sure \'Question Type\' is set to choice only.');
 		this.running = false;
-	} 
+	}
 	else if (this.mode() == 'other') this.next();
 }
 
@@ -870,16 +870,18 @@ Learn.prototype.next = () => {
 
 Learn.prototype.questions = () => {
     let assistant = document.getElementsByClassName('AssistantMultipleChoiceQuestionPromptView-termOption'),
-        learn = document.getElementsByClassName('LearnMultipleChoiceQuestionPrompt-termOption');
+        learn = document.getElementsByClassName('LearnMultipleChoiceQuestionPrompt-termOption'),
+        mc = document.getElementsByClassName('MultipleChoiceQuestionPrompt-termOption');
 
-	return assistant.length ? assistant : learn;
+	return [...assistant, ...learn, ...mc];
 }
 
 Learn.prototype.parent = () => {
     let assistant = document.getElementsByClassName('AssistantMultipleChoiceQuestionPromptView-promptArea'),
-        learn = document.getElementsByClassName('LearnMultipleChoiceQuestionPrompt-promptArea');
+        learn = document.getElementsByClassName('LearnMultipleChoiceQuestionPrompt-promptArea'),
+        mc = document.getElementsByClassName('MultipleChoiceQuestionPrompt-promptArea');
 
-	return assistant.length ? assistant[0] : learn[0];
+	return [...assistant, ...learn, ...mc][0];
 }
 
 Learn.prototype.image = function () {
@@ -894,7 +896,7 @@ Learn.prototype.image = function () {
 Learn.prototype.text = function () {
 	if (this.questions().length == 0) return false;
 
-	let container = this.parent().getElementsByClassName('LearnPromptTextWithImage')[0],
+	let container = this.parent().getElementsByClassName('PromptTextWithImage')[0],
 		parent = container.getElementsByClassName('FormattedText')[0];
 	
 	if (!parent) return document.createElement('div');
@@ -903,7 +905,7 @@ Learn.prototype.text = function () {
 }
 
 Learn.prototype.mode = () => {
-	if (document.getElementsByClassName('AssistantMultipleChoiceQuestionPromptView-termOption').length > 0 || document.getElementsByClassName('LearnMultipleChoiceQuestionPrompt-termOption').length > 0) return 'choice';
+	if (document.getElementsByClassName('AssistantMultipleChoiceQuestionPromptView-termOption').length > 0 || document.getElementsByClassName('LearnMultipleChoiceQuestionPrompt-termOption').length > 0 || document.getElementsByClassName('MultipleChoiceQuestionPrompt-termOptions').length > 0) return 'choice';
 	if (document.getElementsByClassName('AutoExpandTextarea-textarea').length > 0) return 'written';
 	if (document.getElementsByClassName('FlippableFlashcard').length > 0) return 'flashcards';
 

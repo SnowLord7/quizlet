47e0350808767785c6864999810328ba08db00f8
SnowLord <31230715+SnowLord7@users.noreply.github.com>
2020-05-23 08:22:01 -0700

    Fixed Write

--- live.js
+++ live.js
@@ -1,5 +1,5 @@
 /*! DrewSnow v0.0.1 | (c) https://github.com/SnowLord7 */
-let _extensionVersion = 1588187114552;
+let _extensionVersion = 1590247310141;
 
 /**
  * Library by Drew Snow for miscellaneous uses 
@@ -1891,24 +1891,53 @@ Spell.prototype.solve = function (e) {
 	}
 }();
 function Write() {
-	this.speed = 50;
+	this.speed = 100;
 	this.solve();
 }
 
 Write.prototype.solve = function () {
-	let element = document.getElementsByClassName('LearnModeProgressBar-value')[0];
+	let element = document.getElementsByClassName('WriteProgress-value')[0];
+
+    let index = 0,
+        phrases = 'Hello my friend you are you today? I am extremely sad because Quizlet finds enjoyment at my pain, not to mention they are being paid to make me sad while all I gain is depression.'.split(' ');
 
 	if (element) {
 		let remaining = Number(element.innerText) || 0;
 
 		for (let i = 0; i < remaining; i++) {
 			setTimeout(() => {
-				document.getElementById('user-answer').value = 'abcdefghijk';
-				document.getElementById('js-learnModeAnswerButton').click();
-				document.getElementsByClassName('js-learnModeOverrideIncorrect')[0].click();
-			}, i * this.speed);
+				
+                let index = 0,
+                    phrases = 'Hello my friend you are you today? I am extremely sad because Quizlet finds enjoyment at my pain, not to mention they are being paid to make me sad while all I gain is depression.'.split(' ');
+
+                let ta = document.querySelector('textarea'),
+                    value = ' ' + phrases[index];
+
+                if (!ta) return;
+
+                index++;
+                if (index > phrases.length) index = 0;
+
+                ta.focus();
+
+                let val = Object.getOwnPropertyDescriptor(ta.constructor.prototype, 'value');
+                (null == val ? void 0 : val.set) ? val.set.call(ta, value) : ta.value = value;
+
+                let evt = window.document.createEvent('Event');
+                evt.initEvent('input', true, true);
+                ta.dispatchEvent(evt);
+
+                ta.selectionStart = ta.value.length + 1;
+                ta.selectionEnd = ta.value.length + 1;
+
+                document.querySelector('button[type="submit"]').click();
+                document.getElementsByClassName('IncorrectWrittenFeedbackItem-mistyped')[0].querySelector('button').click();
+                        
+            }, i * this.speed);
 		}
-	}
+	} else {
+        alert('Error: Please message the developer that Quizlet has updated.');
+    }
 }
 function Exploit() {
 	this.version = '0.0.1';
